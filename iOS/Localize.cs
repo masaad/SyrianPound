153cc204e9bc3e56df503f9e2f82f47b935ae53a
Moe Asaad <moe.asaad@gmail.com>
2015-05-16 14:06:34 -0500

    Localization Fix

---
+++ iOS/Localize.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms; 
+using Foundation; 
+
+[assembly:Dependency(typeof(SyrianPound.iOS.Localize))]
+namespace SyrianPound.iOS
+{
+	public class Localize : SyrianPound.ILocalize
+	{
+		public System.Globalization.CultureInfo GetCurrentCultureInfo ()
+		{
+			var netLanguage = "en";
+			if (NSLocale.PreferredLanguages.Length > 0) {
+				var pref = NSLocale.PreferredLanguages [0];
+				netLanguage = pref.Replace ("_", "-"); // turns pt_BR into pt-BR
+			}
+			return new System.Globalization.CultureInfo(netLanguage);
+		}
+	}
+}
+

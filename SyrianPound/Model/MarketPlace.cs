1a7e005b9f6086c4af505255d18afb2c4e36bd52
Moe Asaad <moe.asaad@gmail.com>
2015-05-17 01:09:40 -0500

    Main Page Clean up

---
+++ SyrianPound/Model/MarketPlace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SyrianPound
+{
+	public class MarketPlace : IMarketPlace
+	{
+		private Guid _id; 
+		public MarketPlace (Guid id)
+		{
+			_id = id; 
+		}
+
+		public Guid Id 
+		{
+			get { return _id; }  
+		} 
+
+		public string Name { get; set;} 
+
+	}
+}
+

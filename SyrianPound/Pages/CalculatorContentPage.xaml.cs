8af9912c7ae66efa9aab9668b25f00bab3b36746
Moe Asaad <moe.asaad@gmail.com>
2015-06-08 21:54:23 -0500

    Adds Switch control

--- SyrianPound/Pages/CalculatorContentPage.xaml.cs
+++ SyrianPound/Pages/CalculatorContentPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using SyrianPound.Resources;
 using Xamarin.Forms;
 
 namespace SyrianPound
@@ -10,7 +10,20 @@ namespace SyrianPound
 		public CalculatorContentPage ()
 		{
 			InitializeComponent ();
+		    SwitchLabel.Text = AppResources.LblBuying; 
 		}
+
+	    private CalculatorViewModel ViewModel
+	    {
+            get { return BindingContext as CalculatorViewModel; }
+	    }
+
+	    private void SwitchKey_OnToggled(object sender, ToggledEventArgs e)
+	    {
+	        SwitchLabel.Text = e.Value
+	            ? AppResources.LblSelling
+	            : AppResources.LblBuying; 
+	    }
 	}
 }
 
